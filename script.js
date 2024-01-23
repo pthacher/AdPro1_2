bde26d68e3b53ee75fd65ab2f7cae99cb1af881c
pthacher <157059244+pthacher@users.noreply.github.com>
2024-01-23 17:26:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var mymap = L.map('mapid').setView([37.7,-122.4], 10);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map tiles by Stamen Design, under CC BY 3.0.',
-    maxZoom: 18
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
 var ratIcon = L.icon({
